69f40737a060c8efe22d34afd06a440d06833c55
Fabian <fabian@mathysli.ch>
2024-10-26 00:59:56 +0200

    mapping of components and input filters

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,68 @@
 import Header from "./components/header/Header.tsx";
-import Template from "./artworks/1_Template/Template.tsx";
-import {useState} from "react";
+import {CSSProperties, useState} from "react";
 import {EArtworkTags} from "./artworks/0_artworks-list/artworkTags.ts";
+import artworksList from "./artworks/0_artworks-list/artworksList.tsx";
 
 function App() {
     const [currentTag, setCurrentTag] = useState<EArtworkTags>();
     const [search, setSearch] = useState("");
 
-    console.log(currentTag, search)
+    const filteredArtworks = artworksList
+        .filter(artwork => currentTag ? artwork.tags?.includes(currentTag) : true)
+        .filter(artwork => search
+            ? (
+                artwork.tags?.some(tag => tag.toLowerCase().includes(search.toLowerCase())) ||
+                artwork.titel.toLowerCase().includes(search.toLowerCase()) ||
+                artwork.description.toLowerCase().includes(search.toLowerCase()) ||
+                artwork.gitHubName.toLowerCase().includes(search.toLowerCase()) ||
+                artwork.editedAt?.toLocaleDateString().toLowerCase().includes(search.toLowerCase())
+            )
+            : true
+        );
 
-  return (
-    <>
-      <Header
-          setCurrentTag={setCurrentTag}
-          setSearch={setSearch}
-          currentTag={currentTag}
-          search={search}
-      />
-        <div className={"container"}>
-            <Template />
-        </div>
-    </>
-  )
+    return (
+        <>
+            <Header
+                setCurrentTag={setCurrentTag}
+                setSearch={setSearch}
+                currentTag={currentTag}
+                search={search}
+            />
+            <div className={"container"}>
+                <div className={"component-list"}>
+                    {filteredArtworks.length > 0 ?
+                        filteredArtworks.map(artwork => {
+                            artwork.backgroundColor = artwork.backgroundColor ?? "#000"
+
+                            return (
+                                <div
+                                    key={artworksList.indexOf(artwork)}
+                                    className={"component"}
+                                    style={
+                                        artwork.backgroundColor.length <= 7
+                                            ? {"--background-color": artwork.backgroundColor} as CSSProperties
+                                            : {}
+                                    }
+                                >
+                                    {artwork.component}
+                                </div>
+                            )
+                        }) : (
+                            <div className={"no-components-found"}>
+                                <span>No Components where found corresponding to the current filter settings.</span>
+                                <button onClick={() => {
+                                    setCurrentTag(undefined);
+                                    setSearch("");
+                                }}>
+                                    Reset Filters
+                                </button>
+                            </div>
+                        )
+                    }
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default App
--- src/artworks/0_artworks-list/artworksList.tsx
+++ src/artworks/0_artworks-list/artworksList.tsx
@@ -1,17 +1,48 @@
 import {ReactNode} from "react";
 import Template from "../1_Template/Template.tsx"
+import {EArtworkTags} from "./artworkTags.ts";
 
 export interface IArtworkItem {
     component: ReactNode;
-    githubName: string;
+    titel: string;
+    description: string;
+    gitHubName: string;
+    tags?: EArtworkTags[] | string[]
+    backgroundColor?: `#${string}`;     // No transparent backgrounds
     oneLink?: string;
+    editedAt?: Date;
 }
 
 const artworksList: IArtworkItem[] = [
     {
         component: <Template />,
-        githubName: "FabDonRixos",
-        oneLink: "https://fabian.li"
+        titel: "circle",
+        description: "round circle",
+        gitHubName: "FabDonRixos",
+        tags: [EArtworkTags.SIMPLE],
+        backgroundColor: "#000",
+        oneLink: "https://fabian.li",
+        editedAt: new Date("2024-10-26")
+    },
+    {
+        component: <Template />,
+        titel: "circle",
+        description: "round circle",
+        gitHubName: "FabDonRixos",
+        tags: [EArtworkTags.SIMPLE],
+        backgroundColor: "#000",
+        oneLink: "https://fabian.li",
+        editedAt: new Date("2024-10-26")
+    },
+    {
+        component: <Template />,
+        titel: "circle",
+        description: "round circle",
+        gitHubName: "FabDonRixos",
+        tags: [EArtworkTags.SIMPLE],
+        backgroundColor: "#000",
+        oneLink: "https://fabian.li",
+        editedAt: new Date("2024-10-26")
     }
 ]
 
