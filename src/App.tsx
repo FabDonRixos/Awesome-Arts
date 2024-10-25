82305c7b9aee3999d06d051f3bcc7bb054dc898e
Fabian <fabian@mathysli.ch>
2024-10-25 23:53:06 +0200

    inputs for header and responsive header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import Header from "./components/header/Header.tsx";
 import Template from "./artworks/1_Template/Template.tsx";
+import {useState} from "react";
+import {EArtworkTags} from "./artworks/0_artworks-list/artworkTags.ts";
 
 function App() {
+    const [currentTag, setCurrentTag] = useState<EArtworkTags>();
+    const [search, setSearch] = useState("");
+
+    console.log(currentTag, search)
 
   return (
     <>
-      <Header/>
+      <Header
+          setCurrentTag={setCurrentTag}
+          setSearch={setSearch}
+          currentTag={currentTag}
+          search={search}
+      />
         <div className={"container"}>
             <Template />
         </div>
