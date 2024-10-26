d922018a1e1215f2848c383e563391af764f05cf
FabDonRixos <118833164+FabDonRixos@users.noreply.github.com>
2024-10-26 21:39:07 +0200

    Component adding logic (#2)

--- src/artworks/0_artworks-list/artworksList.tsx
+++ src/artworks/0_artworks-list/artworksList.tsx
@@ -1,18 +1,39 @@
 import {ReactNode} from "react";
 import Template from "../1_Template/Template.tsx"
+import {EArtworkTags} from "./artworkTags.ts";
+
+export interface ILink {
+    name: string;
+    href: string;
+}
 
 export interface IArtworkItem {
     component: ReactNode;
-    githubName: string;
-    oneLink?: string;
+    titel: string;
+    description: string;
+    gitHubName: string;
+    tags?: EArtworkTags[] | string[]
+    backgroundColor?: `#${string}`;     // No transparent backgrounds
+    full?: boolean;                     // Only use this if you really have no other but to claim more space.
+    oneLink?: ILink;
+    editedAt?: Date;
 }
 
 const artworksList: IArtworkItem[] = [
     {
         component: <Template />,
-        githubName: "FabDonRixos",
-        oneLink: "https://fabian.li"
-    }
+        titel: "circle",
+        description: "round circle",
+        gitHubName: "FabDonRixos",
+        tags: [EArtworkTags.SIMPLE],
+        backgroundColor: "#000",
+        full: true,
+        oneLink: {
+            name: "fabian.li",
+            href: "https://fabian.li"
+        },
+        editedAt: new Date("2024-10-26")
+    },
 ]
 
 export default artworksList;
