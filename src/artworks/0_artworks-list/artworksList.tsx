c23c409e4372e33d05a796ec0d00b57635a7bbb5
FabDonRixos <118833164+FabDonRixos@users.noreply.github.com>
2024-10-26 21:43:15 +0200

    Revert "Component adding logic (#2)"
    This reverts commit d922018a1e1215f2848c383e563391af764f05cf.

--- src/artworks/0_artworks-list/artworksList.tsx
+++ src/artworks/0_artworks-list/artworksList.tsx
@@ -1,39 +1,18 @@
 import {ReactNode} from "react";
 import Template from "../1_Template/Template.tsx"
-import {EArtworkTags} from "./artworkTags.ts";
-
-export interface ILink {
-    name: string;
-    href: string;
-}
 
 export interface IArtworkItem {
     component: ReactNode;
-    titel: string;
-    description: string;
-    gitHubName: string;
-    tags?: EArtworkTags[] | string[]
-    backgroundColor?: `#${string}`;     // No transparent backgrounds
-    full?: boolean;                     // Only use this if you really have no other but to claim more space.
-    oneLink?: ILink;
-    editedAt?: Date;
+    githubName: string;
+    oneLink?: string;
 }
 
 const artworksList: IArtworkItem[] = [
     {
         component: <Template />,
-        titel: "circle",
-        description: "round circle",
-        gitHubName: "FabDonRixos",
-        tags: [EArtworkTags.SIMPLE],
-        backgroundColor: "#000",
-        full: true,
-        oneLink: {
-            name: "fabian.li",
-            href: "https://fabian.li"
-        },
-        editedAt: new Date("2024-10-26")
-    },
+        githubName: "FabDonRixos",
+        oneLink: "https://fabian.li"
+    }
 ]
 
 export default artworksList;
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,67 +1,33 @@
 import {IconLogo, IconFabian, IconGithub, IconLinkedin} from "../../assets/iconLibrary.ts";
 import style from "./header.module.scss";
-import {EArtworkTags} from "../../artworks/0_artworks-list/artworkTags.ts";
 
-interface InputsProps {
-    setCurrentTag: (currentTag: EArtworkTags) => void;
-    setSearch: (search: string) => void;
-    currentTag?: EArtworkTags;
-    search: string;
-}
-
-export default function Header(props: InputsProps) {
+export default function Header() {
 
     return (
         <header className={style.header}>
-            <div className={style.top} >
-                <div className={style.left}>
-                    <IconLogo className={style.logo}/>
-                    <h1>Awesome Arts</h1>
-                </div>
-                <div className={style.right}>
-                    <Inputs {...props} />
-                    <div className={style.links}>
-                        <a href="https://github.com/FabDonRixos/awesome-arts" target={"_blank"}>
-                            <IconGithub/>
-                        </a>
-                        <a href="https://www.linkedin.com/in/fabian-mathys-42a595332/" target={"_blank"}>
-                            <IconLinkedin/>
-                        </a>
-                        <a href="https://fabian.li" target={"_blank"}>
-                            <IconFabian/>
-                        </a>
-                    </div>
-                </div>
+            <div className={style.left}>
+                <IconLogo className={style.logo}/>
+                <h1>Awesome Arts</h1>
             </div>
-            <div className={style.bottom} >
-                <Inputs {...props} />
+            <div className={style.right}>
+                <div className={style.search}>
+
+                </div>
+                <div className={style.filters}>
+
+                </div>
+                <div className={style.links}>
+                    <a href="https://github.com/FabDonRixos/awesome-arts" target={"_blank"}>
+                        <IconGithub/>
+                    </a>
+                    <a href="https://www.linkedin.com/in/fabian-mathys-42a595332/" target={"_blank"}>
+                        <IconLinkedin/>
+                    </a>
+                    <a href="https://fabian.li" target={"_blank"}>
+                        <IconFabian/>
+                    </a>
+                </div>
             </div>
         </header>
     );
-}
-
-function Inputs({setCurrentTag, setSearch, currentTag, search}: InputsProps) {
-    return (
-        <>
-            <input
-                className={style.search}
-                type="text"
-                placeholder={"Search"}
-                value={search}
-                onChange={e => setSearch(e.target.value)}/>
-
-            <select
-                className={style.filters}
-                name={"tagSelection"}
-                defaultValue={""}
-                value={currentTag}
-                onChange={e => setCurrentTag(e.target.value as EArtworkTags)}
-            >
-                <option value={""}>Select a Tag</option>
-                {Object.entries(EArtworkTags).map(([key, value]) =>
-                    <option key={key} label={key} value={value}/>
-                )}
-            </select>
-        </>
-    )
 }
