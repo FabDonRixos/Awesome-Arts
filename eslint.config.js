26eea852fab1c3bd70ffaeb5f7a31f8cdee978da
Fabian <fabian@mathysli.ch>
2024-11-07 00:01:58 +0100

    #24 configure eslint and prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,43 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import eslintPluginReact from "eslint-plugin-react";
+import eslintPluginReactHooks from "eslint-plugin-react-hooks";
+import eslintPluginTypescript from "@typescript-eslint/eslint-plugin";
+import eslintPluginReactRefresh from "eslint-plugin-react-refresh";
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import eslintParser from "@typescript-eslint/parser";
+import eslintPluginHtml from "eslint-plugin-html";
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+export default [
+    {
+        files: ["**/*.{ts,tsx,js,jsx,html}"],
+        ignores: ["dist/"],
+        languageOptions: {
+            parser: eslintParser,
+            parserOptions: {
+                ecmaVersion: 2020,
+                sourceType: "module",
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+        },
+        settings: {
+            react: {
+                version: "detect",
+            },
+        },
+        plugins: {
+            "react-hooks": eslintPluginReactHooks,
+            "react-refresh": eslintPluginReactRefresh,
+            "@typescript-eslint": eslintPluginTypescript,
+            react: eslintPluginReact,
+            html: eslintPluginHtml,
+            prettier: eslintPluginPrettier,
+        },
+        rules: {
+            "prettier/prettier": "error",
+            "react/react-in-jsx-scope": "off",
+            "@typescript-eslint/explicit-module-boundary-types": "off",
+            "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+        },
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
-)
+];
