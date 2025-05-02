311a7e2fb90dc1c606e999646557685f94017d32
3epge <3epge@proton.me>
2025-05-03 01:23:53 +0800

    Add artwork EvilEye

--- src/artworks/0_artworks-list/lazyLoad.ts
+++ src/artworks/0_artworks-list/lazyLoad.ts
@@ -4,3 +4,4 @@ export const YinYang = lazy(() => import("../YinYang/YinYang.tsx"));
 export const ColorModel = lazy(() => import("../ColorModel/ColorModel.tsx"));
 export const Cube = lazy(() => import("../Cube/Cube.tsx"));
 export const AtomModel = lazy(() => import("../AtomModel/AtomModel.tsx"));
+export const EvilEye = lazy(() => import("../EvilEye/EvilEye.tsx"));
---
+++ src/artworks/EvilEye/EvilEye.tsx
@@ -0,0 +1,22 @@
+import { ReactElement } from "react";
+import style from "./evileye.module.scss";
+
+export default function Template(): ReactElement {
+    return (
+        <label className={style.evilEyeWrapper}>
+            <div className={style.navy}>
+                <div className={style.highlight} />
+                <div className={style.navyShape} />
+            </div>
+            <div className={style.white}>
+                <div className={style.whiteShape} />
+            </div>
+            <div className={style.blue}>
+                <div className={style.blueShape} />
+            </div>
+            <div className={style.black}>
+                <div className={style.blackShape} />
+            </div>
+        </label>
+    );
+}
